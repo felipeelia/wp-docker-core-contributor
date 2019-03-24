06c60801b1a9352790af92a70abee5d9e10c5a34
Felipe Elia <felipe.elia@gmail.com>
2019-03-24 14:26:42 -0300

    Enable HTTPS Fix node user Install PHPUnit Make Apache container waits for MariaDB

--- .docker/wordpress/docker-entrypoint.sh
+++ .docker/wordpress/docker-entrypoint.sh
@@ -7,11 +7,14 @@ if ! [ -e src/index.php ]; then
     chown -R www-data:www-data .
 fi
 
-if ! [ -e src/wp-config.php ]; then
-    wp core config --dbhost=db --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --extra-php <<PHP
-define( 'WP_DEBUG', true );
-PHP
-fi
+wait-for-it -t 0 db:3306
+
+# Commented until I find a way to wait until the build process is done.
+# if ! [ -e src/wp-config.php ]; then
+#     wp core config --dbhost=db --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --extra-php <<PHP
+# define( 'WP_DEBUG', true );
+# PHP
+# fi
 
 svn up
 
